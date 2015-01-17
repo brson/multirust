690597f6460f53fe48190321e9aa3b121aee80d2
Brian Anderson <banderson@mozilla.com>
2015-01-16 16:44:39 -0800

    Prepend secret flags. #15

--- test/mock.sh
+++ test/mock.sh
@@ -4,6 +4,8 @@ template_bin_name="@@TEMPLATE_BIN_NAME@@"
 template_version="@@TEMPLATE_VERSION@@"
 template_hash="@@TEMPLATE_HASH@@"
 
-if [ "$1" = "--version" ]; then
-    echo "$template_bin_name $template_version ($template_hash 2014-12-24 20:47:12 +0000)"
-fi
+for arg in "$@"; do
+    if [ "$arg" = "--version" ]; then
+	echo "$template_bin_name $template_version ($template_hash 2014-12-24 20:47:12 +0000)"
+    fi
+done
