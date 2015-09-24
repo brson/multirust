ae8f638a27468b932caf7f8c31da3dfc8d26b70c
Brian Anderson <banderson@mozilla.com>
2015-09-23 17:29:56 -0700

    Untabify blastoff.sh

--- blastoff.sh
+++ blastoff.sh
@@ -57,79 +57,79 @@ get_architecture() {
     verbose_say "uname -m reports: $_cputype"
 
     if [ "$_ostype" = Darwin -a "$_cputype" = i386 ]; then
-	# Darwin `uname -s` lies
-	if sysctl hw.optional.x86_64 | grep -q ': 1'; then
-	    local _cputype=x86_64
-	fi
+        # Darwin `uname -s` lies
+        if sysctl hw.optional.x86_64 | grep -q ': 1'; then
+            local _cputype=x86_64
+        fi
     fi
 
     case "$_ostype" in
 
-	Linux)
-	    local _ostype=unknown-linux-gnu
-	    ;;
+        Linux)
+            local _ostype=unknown-linux-gnu
+            ;;
 
-	FreeBSD)
-	    local _ostype=unknown-freebsd
-	    ;;
+        FreeBSD)
+            local _ostype=unknown-freebsd
+            ;;
 
-	DragonFly)
-	    local _ostype=unknown-dragonfly
-	    ;;
+        DragonFly)
+            local _ostype=unknown-dragonfly
+            ;;
 
-	Darwin)
-	    local _ostype=apple-darwin
-	    ;;
+        Darwin)
+            local _ostype=apple-darwin
+            ;;
 
-	MINGW* | MSYS*)
-	    local _ostype=pc-windows-gnu
-	    ;;
+        MINGW* | MSYS*)
+            local _ostype=pc-windows-gnu
+            ;;
 
-	*)
-	    err "unrecognized OS type: $_ostype"
-	    ;;
+        *)
+            err "unrecognized OS type: $_ostype"
+            ;;
 
     esac
 
     case "$_cputype" in
 
-	i386 | i486 | i686 | i786 | x86)
+        i386 | i486 | i686 | i786 | x86)
             local _cputype=i686
             ;;
 
-	xscale | arm)
-	    local _cputype=arm
+        xscale | arm)
+            local _cputype=arm
             ;;
 
-	armv7l)
+        armv7l)
             local _cputype=arm
             local _ostype="${_ostype}eabihf"
             ;;
 
-	x86_64 | x86-64 | x64 | amd64)
+        x86_64 | x86-64 | x64 | amd64)
             local _cputype=x86_64
             ;;
 
-	*)
+        *)
             err "unknown CPU type: $CFG_CPUTYPE"
 
     esac
 
     # Detect 64-bit linux with 32-bit userland
     if [ $_ostype = unknown-linux-gnu -a $_cputype = x86_64 ]; then
-	# $SHELL does not exist in standard 'sh', so probably only exists
-	# if configure is running in an interactive bash shell. /usr/bin/env
-	# exists *everywhere*.
-	local _bin_to_probe="${SHELL-bogus_shell}"
-	if [ ! -e "$_bin_to_probe" -a -e "/usr/bin/env" ]; then
-	    _bin_to_probe="/usr/bin/env"
-	fi
-	if [ -e "$_bin_to_probe" ]; then
-	    file -L "$_bin_to_probe" | grep -q "x86[_-]64"
-	    if [ $? != 0 ]; then
-		local _cputype=i686
-	    fi
-	fi
+        # $SHELL does not exist in standard 'sh', so probably only exists
+        # if configure is running in an interactive bash shell. /usr/bin/env
+        # exists *everywhere*.
+        local _bin_to_probe="${SHELL-bogus_shell}"
+        if [ ! -e "$_bin_to_probe" -a -e "/usr/bin/env" ]; then
+            _bin_to_probe="/usr/bin/env"
+        fi
+        if [ -e "$_bin_to_probe" ]; then
+            file -L "$_bin_to_probe" | grep -q "x86[_-]64"
+            if [ $? != 0 ]; then
+                local _cputype=i686
+            fi
+        fi
     fi
 
     local _arch="$_cputype-$_ostype"
